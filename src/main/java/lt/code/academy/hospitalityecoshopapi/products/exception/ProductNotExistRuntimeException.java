7cf53b8fc3c3a161ea15f82bb80c189dbe966b86
vildaraite <119286337+vildaraite@users.noreply.github.com>
2023-06-19 19:35:38 +0300

    eco_shop-ui and api parts add by upload

---
+++ src/main/java/lt/code/academy/hospitalityecoshopapi/products/exception/ProductNotExistRuntimeException.java
@@ -0,0 +1,12 @@
+package lt.code.academy.hospitalityecoshopapi.products.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class ProductNotExistRuntimeException extends RuntimeException {
+    private final UUID id;
+}
